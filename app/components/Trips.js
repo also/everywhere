a4c1485f2492e498318911674d34323982937031
Ryan Berdeen <ryan@ryanberdeen.com>
2015-06-22 03:11:48 -0400

    stroke animation

--- app/components/Trips.js
+++ app/components/Trips.js
@@ -1,9 +1,39 @@
 import * as React from 'react';
+import d3 from 'd3';
 
 
 const Trip = React.createClass({
   contextTypes: {
     path: React.PropTypes.any
+  },
+
+  startAnimating() {
+    let run = true;
+    const stopAnimating = () => run = false;
+    this._stopAnimating = stopAnimating;
+    const duration = 7000;
+    const node = React.findDOMNode(this);
+    const length = node.getTotalLength();
+    const ease = d3.ease('linear');
+    d3.timer((elapsed) => {
+      const t = elapsed / duration;
+
+      const e = ease(t);
+      const l = e * length;
+
+      node.setAttribute('stroke-dasharray', `${l},${length}`);
+
+      if (t > 1) {
+        stopAnimating();
+      }
+      return !run;
+    });
+  },
+
+  stopAnimating() {
+    if (this._stopAnimating) {
+      this._stopAnimating();
+    }
   },
 
   render() {
@@ -16,6 +46,11 @@ const Trip = React.createClass({
     // react doesn't support mask
     // https://github.com/facebook/react/issues/1657#issuecomment-63209488
     React.findDOMNode(this).setAttribute('mask', 'url(#boundary-mask)');
+    this.startAnimating();
+  },
+
+  componentWillUnmount() {
+    this.stopAnimating();
   }
 });
 
