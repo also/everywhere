6b4308ac0ddca53240b1d725e4f9d402da5b2415
Ryan Berdeen <ryan@ryanberdeen.com>
2015-07-04 15:46:26 -0400

    very useful treemap viz

--- app/components/WayList.js
+++ app/components/WayList.js
@@ -1,7 +1,51 @@
-import * as React from 'react';
+import * as React from 'react/addons';
 import {Link} from 'react-router';
+import d3 from 'd3';
 import MapComponent from './Map';
 
+
+const Tree = React.createClass({
+  mixins: [React.addons.PureRenderMixin],
+
+  contextTypes: {
+    projection: React.PropTypes.any
+  },
+
+  propTypes: {
+    tree: React.PropTypes.any.isRequired
+  },
+
+  render() {
+    const {tree} = this.props;
+    const {projection} = this.context;
+
+    const rects = [];
+
+    const visit = node => {
+      const {extent} = node;
+      const coords = [
+        [extent[0][0], extent[0][1]],
+        [extent[1][0], extent[0][1]],
+        [extent[1][0], extent[1][1]],
+        [extent[0][0], extent[1][1]],
+        [extent[0][0], extent[0][1]]
+      ].map(projection);
+      rects.push(<path className="extent" d={d3.svg.line()(coords)}/>);
+
+      if (node.children) {
+        node.children.forEach(visit);
+      }
+    };
+
+    visit(tree);
+
+    return (
+      <g>
+        {rects}
+      </g>
+    );
+  }
+});
 
 export default React.createClass({
   getInitialState() {
@@ -24,7 +68,7 @@ export default React.createClass({
     return (
       <div>
         <MapComponent width="1000" height="1000" onMouseMove={this.onMouseMove} selectedStreetName={this.state.selectedStreetName}>
-          {() => null}
+          {this.mapLayers}
         </MapComponent>
         <ul style={{WebkitColumnWidth: '200px'}}>
           {groupedWays.map(way => (
@@ -33,5 +77,10 @@ export default React.createClass({
         </ul>
       </div>
     );
+  },
+
+  mapLayers() {
+    const {wayTree} = this.props;
+    return <Tree tree={wayTree}/>;
   }
 });
