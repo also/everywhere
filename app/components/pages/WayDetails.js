bb33470830d80e1cebba9c6f222bfb64a2155037
Ryan Berdeen <ryan@ryanberdeen.com>
2021-04-29 14:35:38 -0400

    another function component

--- app/components/pages/WayDetails.js
+++ app/components/pages/WayDetails.js
@@ -9,52 +9,44 @@ import Ways from '../Ways';
 import WayListColumns from '../WayListColumns';
 import MapBox from '../MapBox';
 
-export default createReactClass({
-  render() {
-    const { way } = this.props;
-    const intersections = new Set();
+export default function WayDetails({ way }) {
+  const intersections = new Set();
 
-    way.features.forEach(feat => {
-      feat.intersections.forEach(intersection => {
-        intersection.ways.forEach(iway =>
-          intersections.add(iway.properties.displayName)
-        );
-      });
+  way.features.forEach(feat => {
+    feat.intersections.forEach(intersection => {
+      intersection.ways.forEach(iway =>
+        intersections.add(iway.properties.displayName)
+      );
     });
+  });
 
-    intersections.delete(way.displayName);
+  intersections.delete(way.displayName);
 
-    return (
-      <div>
-        <PageTitle>{way.displayName}</PageTitle>
-        <MapBox>
-          <MapComponent
-            width={400}
-            height={400}
-            zoomFeature={featureCollection(way.features)}
-            zoom={0.7}
-          >
-            {this.mapLayers}
-          </MapComponent>
-        </MapBox>
-        <h2>Intersections</h2>
-        <WayListColumns>
-          {Array.from(intersections)
-            .sort()
-            .map(int => (
-              <li>
-                <Link key={int} to={`/ways/${int}`}>
-                  {int}
-                </Link>
-              </li>
-            ))}
-        </WayListColumns>
-      </div>
-    );
-  },
-
-  mapLayers() {
-    const { way } = this.props;
-    return <Ways features={way.features} selected={true} />;
-  },
-});
+  return (
+    <div>
+      <PageTitle>{way.displayName}</PageTitle>
+      <MapBox>
+        <MapComponent
+          width={400}
+          height={400}
+          zoomFeature={featureCollection(way.features)}
+          zoom={0.7}
+        >
+          {() => <Ways features={way.features} selected={true} />}
+        </MapComponent>
+      </MapBox>
+      <h2>Intersections</h2>
+      <WayListColumns>
+        {Array.from(intersections)
+          .sort()
+          .map(int => (
+            <li>
+              <Link key={int} to={`/ways/${int}`}>
+                {int}
+              </Link>
+            </li>
+          ))}
+      </WayListColumns>
+    </div>
+  );
+}
