2f0df54418e89a7e71abc3e2d53949417f6d938f
Ryan Berdeen <ryan@ryanberdeen.com>
2021-04-29 12:44:10 -0400

    some d3 types

--- app/components/MapContext.ts
+++ app/components/MapContext.ts
@@ -1,3 +1,7 @@
+import d3 from 'd3';
 import React from 'react';
 
-export default React.createContext(undefined);
+export default React.createContext<{
+  projection: d3.geo.Projection;
+  path: d3.geo.Path;
+}>(undefined as any);
