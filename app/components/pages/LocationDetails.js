5079ffe951ed4d3a5c270a7a42637efd7382bf9e
Ryan Berdeen <ryan@ryanberdeen.com>
2021-04-29 13:08:15 -0400

    cleanup

--- app/components/pages/LocationDetails.js
+++ app/components/pages/LocationDetails.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
 import { useHistory } from 'react-router-dom';
 
 import * as format from '../../format';
--- app/components/pages/TripListPage.js
+++ app/components/pages/TripListPage.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import PageTitle from '../PageTitle';
 import TripList from '../TripList';
 
