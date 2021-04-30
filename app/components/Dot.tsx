fd5d115575e0a2f1a9e67ee28c8421b8e4941cfe
Ryan Berdeen <ryan@ryanberdeen.com>
2021-04-30 16:30:13 -0400

    convert video details to hooks and TS

--- app/components/Dot.tsx
+++ app/components/Dot.tsx
@@ -2,7 +2,12 @@ import { useContext } from 'react';
 
 import MapContext from './MapContext';
 
-export default function Dot({ position, ...extraProps }) {
+export default function Dot({
+  position,
+  ...extraProps
+}: {
+  position: [number, number];
+} & React.SVGAttributes<SVGCircleElement>) {
   const { projection } = useContext(MapContext);
 
   if (position) {
---
+++ app/components/pages/VideoDetails.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+
+import * as format from '../../format';
+import { findSeekPosition, Video } from '../../videos';
+import { featureCollection } from '../../geo';
+
+import PageTitle from '../PageTitle';
+import TripList from '../TripList';
+import VideoPlayer from '../VideoPlayer';
+import MapComponent from '../Map';
+import Trips from '../Trips';
+import Dot from '../Dot';
+import MapBox from '../MapBox';
+
+function VideoAndMap({ video, seek }: { video: Video; seek: number }) {
+  const history = useHistory();
+  const [location, setLocation] = useState<[number, number]>([0, 0]);
+
+  function onClick({ geo }: { geo: [number, number] }) {
+    history.push(`/videos/${video.name}/${findSeekPosition(video, geo)}`);
+  }
+
+  return (
+    <span>
+      <span style={{ display: 'inline-block' }}>
+        <VideoPlayer video={video} seek={seek} onLocationChange={setLocation} />
+      </span>
+      <MapBox
+        style={{
+          display: 'inline-block',
+          verticalAlign: 'top',
+          marginLeft: '1em',
+        }}
+      >
+        <MapComponent
+          width={360}
+          height={360}
+          zoomFeature={featureCollection(video.coverage)}
+          onClick={onClick}
+        >
+          <Trips trips={video.coverage} />
+          <Dot r={4} className="position" position={location} />
+        </MapComponent>
+      </MapBox>
+    </span>
+  );
+}
+
+export default function VideoDetails({
+  video,
+  seek,
+}: {
+  video: Video;
+  seek: number;
+}) {
+  return (
+    <div>
+      <PageTitle>{video.name}</PageTitle>
+      <p>
+        Taken <strong>{video.start.format('LLL')}</strong>,{' '}
+        {format.duration(video.duration)} long
+      </p>
+      <VideoAndMap video={video} seek={seek} />
+      <h2>Trips</h2>
+      <TripList trips={video.trips} />
+      <h2>Stills</h2>
+      <div>
+        {video.stills.map(({ small, large }, i) => (
+          <a href={large} key={i}>
+            <img src={large} width="320" />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
