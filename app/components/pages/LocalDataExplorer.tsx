24094f4174bffba98a99a07a71cc064a906fd1f4
Ryan Berdeen <ryan@ryanberdeen.com>
2021-05-17 11:14:04 -0400

    use mapbox tiles

--- app/components/pages/LocalDataExplorer.tsx
+++ app/components/pages/LocalDataExplorer.tsx
@@ -74,10 +74,20 @@ function LeafletComponent({ features }: { features: GeoJSON.Feature[] }) {
     }
     const map = mapRef.current;
     map.eachLayer((layer) => map.removeLayer(layer));
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer(
+      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      {
       attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          'pk.eyJ1IjoicmJlcmRlZW4iLCJhIjoiZTU1YjNmOWU4MWExNDJhNWNlMTAxYjA2NjFlODBiNWUifQ.AHJ0I8wQi1pJekXfAaPxLw',
+      }
+    ).addTo(map);
+
     features.forEach((f) =>
       L.geoJSON(f)
         .addTo(map)
