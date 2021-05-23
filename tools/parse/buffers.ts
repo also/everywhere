695a2caa848a0638ac95eeb3bbd3d179390e7221
Ryan Berdeen <ryan@ryanberdeen.com>
2021-05-23 02:52:45 -0400

    SeekableFetchBuffer, for fun

--- tools/parse/buffers.ts
+++ tools/parse/buffers.ts
@@ -50,6 +50,17 @@ export abstract class AbstractSeekableBuffer implements SeekableBuffer {
     this.buf = buf;
     this.offset = 0;
   }
+
+  protected copyTo(result: AbstractSeekableBuffer) {
+    result.buf = new DataView(
+      result.arrayBuffer,
+      this.buf.byteOffset,
+      this.buf.byteLength
+    );
+    result.offset = this.offset;
+    result.bufFileOffset = this.bufFileOffset;
+    result.bufLength = this.bufLength;
+  }
 }
 
 export class SeekableBlobBuffer
@@ -76,15 +87,58 @@ export class SeekableBlobBuffer
   view() {
     const result = new SeekableBlobBuffer(this.blob, this.bufferSize);
     result.arrayBuffer = this.arrayBuffer.slice(0);
-    result.buf = new DataView(
-      result.arrayBuffer,
-      this.buf.byteOffset,
-      this.buf.byteLength
-    );
-    result.offset = this.offset;
-    result.bufFileOffset = this.bufFileOffset;
-    result.bufLength = this.bufLength;
+    this.copyTo(result);
 
     return result;
+  }
+}
+
+export class SeekableFetchBuffer
+  extends AbstractSeekableBuffer
+  implements SeekableBuffer
+{
+  protected arrayBuffer: ArrayBuffer;
+
+  constructor(
+    private url: string,
+    public size: number,
+    private bufferSize: number
+  ) {
+    super();
+
+    this.arrayBuffer = new ArrayBuffer(0);
+  }
+
+  protected async _seek(to: number) {
+    this.bufFileOffset = to;
+    this.arrayBuffer = await fetch(this.url, {
+      headers: { range: `bytes=${to}-${to + this.bufferSize}` },
+    }).then((r) => r.arrayBuffer());
+    this.bufLength = this.arrayBuffer.byteLength;
+  }
+
+  view() {
+    const result = new SeekableFetchBuffer(
+      this.url,
+      this.size,
+      this.bufferSize
+    );
+    result.arrayBuffer = this.arrayBuffer.slice(0);
+    this.copyTo(result);
+
+    return result;
+  }
+
+  static async forUrl(
+    url: string,
+    bufferSize: number
+  ): Promise<SeekableFetchBuffer> {
+    const sizeStr = await fetch(url, {
+      method: 'head',
+    }).then((r) => r.headers.get('content-length'));
+    if (!sizeStr) {
+      throw new Error('missing content-length');
+    }
+    return new SeekableFetchBuffer(url, parseInt(sizeStr, 10), bufferSize);
   }
 }
