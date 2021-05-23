14164502de3c478102aee121ce3fcd50c9597673
Ryan Berdeen <ryan@ryanberdeen.com>
2021-05-22 20:44:45 -0400

    fetch more strava trips

--- tools/fetch-strava-trips.ts
+++ tools/fetch-strava-trips.ts
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+
 import axios from 'axios';
 import { FeatureCollection, MultiLineString } from 'geojson';
 import * as topojson from 'topojson';
@@ -22,8 +25,52 @@ const streamNames = [
 
 const otherStreamNames = streamNames.slice(1);
 
+type ActivityType =
+  | 'AlpineSki'
+  | 'BackcountrySki'
+  | 'Canoeing'
+  | 'Crossfit'
+  | 'EBikeRide'
+  | 'Elliptical'
+  | 'Golf'
+  | 'Handcycle'
+  | 'Hike'
+  | 'IceSkate'
+  | 'InlineSkate'
+  | 'Kayaking'
+  | 'Kitesurf'
+  | 'NordicSki'
+  | 'Ride'
+  | 'RockClimbing'
+  | 'RollerSki'
+  | 'Rowing'
+  | 'Run'
+  | 'Sail'
+  | 'Skateboard'
+  | 'Snowboard'
+  | 'Snowshoe'
+  | 'Soccer'
+  | 'StairStepper'
+  | 'StandUpPaddling'
+  | 'Surfing'
+  | 'Swim'
+  | 'Velomobile'
+  | 'VirtualRide'
+  | 'VirtualRun'
+  | 'Walk'
+  | 'WeightTraining'
+  | 'Wheelchair'
+  | 'Windsurf'
+  | 'Workout'
+  | 'Yoga';
+
 type Activity = {
   id: string;
+  type: ActivityType;
+  name: string;
+  start_date: string;
+  elapsed_time: number;
+  moving_time: number;
 };
 
 type StravaCoord = [number, number, ...any];
@@ -94,7 +141,9 @@ type Stream =
   | MovingStream
   | SmoothGradeStream;
 
-type SBT = { [K in Stream['type']]: Extract<Stream, { type: K }>['data'] };
+type SBT = {
+  [K in Stream['type']]?: Extract<Stream, { type: K }>['data'];
+};
 
 type StreamsByType = {
   activity: Activity;
@@ -114,27 +163,64 @@ async function get(path: string) {
   }
 }
 
-export function getTrips() {
-  return get('athlete/activities');
+type SummaryActivity = {
+  id: string;
+  type: ActivityType;
+  name: string;
+};
+
+export async function getTrips(): Promise<SummaryActivity[]> {
+  const result: SummaryActivity[] = [];
+
+  let page = 1;
+
+  while (true) {
+    console.error(`listing activities, page ${page}`);
+    const current: SummaryActivity[] = await get(
+      `athlete/activities?per_page=200&page=${page++}`
+    );
+    if (current.length === 0) {
+      break;
+    }
+    result.push(...current);
+  }
+  return result;
 }
 
 function getTrip(id: string) {
   return get(`activities/${id}`);
 }
 
-function getStreams(activity: Activity): StreamsByType {
-  return get(`activities/${activity.id}/streams/${streamNames.join(',')}`).then(
-    (streams: Stream[]) => {
-      const streamsByType: Partial<StreamsByType> = { activity };
-      streams.forEach((s) => (streamsByType[s.type] = s.data));
-      return streamsByType;
+async function getStreams(
+  activity: Activity
+): Promise<StreamsByType | undefined> {
+  let streams: Stream[];
+  try {
+    streams = await get(
+      `activities/${activity.id}/streams/${streamNames.join(',')}`
+    );
+  } catch (e) {
+    if (e?.response?.status === 404) {
+      return;
     }
-  ) as any;
+    throw e;
+  }
+  const streamsByType: StreamsByType = { activity };
+  streams.forEach((s) => (streamsByType[s.type] = s.data));
+  return streamsByType;
 }
 
-function streamsToGeoJson(
-  streams: StreamsByType
-): FeatureCollection<MultiLineString> {
+type TripGeoJSON = FeatureCollection<
+  MultiLineString,
+  {
+    activity: Activity;
+  }
+>;
+
+function streamsToGeoJson(streams: StreamsByType): TripGeoJSON | undefined {
+  if (!streams.latlng) {
+    return;
+  }
   const orderedStreams: Stream['data'][] = [];
   otherStreamNames.forEach((type) => {
     if (streams[type]) {
@@ -178,17 +264,68 @@ function streamsToGeoJson(
   };
 }
 
-function geoJsonToTopoJson(geoJson) {
+function geoJsonToTopoJson(geoJson: TripGeoJSON) {
   return topojson.topology(
     { geoJson },
     { 'property-transform': (f) => f.properties }
   );
 }
 
-export default async function ({ _: [id] }: { _: string[] }) {
+async function fetchTrip(id: string) {
   const activity = await getTrip(id);
   const streams = await getStreams(activity);
+  if (!streams) {
+    return;
+  }
   const gj = streamsToGeoJson(streams);
-  const result = geoJsonToTopoJson(gj);
-  console.log(JSON.stringify(result));
+  if (gj) {
+    return geoJsonToTopoJson(gj);
+  }
+}
+
+async function fetchAllTrips() {
+  for (const trip of await getTrips()) {
+    const { id, name, type } = trip;
+    const filename = path.join(
+      'app-data',
+      'strava-trips',
+      `strava-${id}.geojson`
+    );
+    if (trip.type === 'Workout') {
+      console.error(`skipping ${type} ${id}`);
+      continue;
+    }
+    if (name.match(/^\d+ min /)) {
+      console.log(`skipping probably peloton "${name}" ${id}`);
+      continue;
+    }
+
+    if (!fs.existsSync(filename)) {
+      console.log(`fetching ${id}`);
+
+      try {
+        const topojson = await fetchTrip(id);
+        if (topojson) {
+          fs.writeFileSync(filename, JSON.stringify(topojson));
+        } else {
+          console.error(`no geo info in "${name}" ${id}`);
+        }
+      } catch (e) {
+        console.log(trip);
+        console.log(e);
+        throw e;
+      }
+    } else {
+      console.log(`already fetched ${id}`);
+    }
+  }
+}
+
+export default async function ({ _: [id] }: { _: string[] }) {
+  if (id) {
+    const result = fetchTrip(id);
+    console.log(JSON.stringify(result));
+  } else {
+    await fetchAllTrips();
+  }
 }
--- tools/list-trips.js
+++ tools/list-trips.js
@@ -1,6 +1,6 @@
 import { getTrips } from './fetch-strava-trips';
 
-export default async function ({ _: [id] }) {
+export default async function () {
   const trips = await getTrips();
   console.log(trips);
 }
