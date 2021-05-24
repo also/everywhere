7188ee1014e271b4f1fe34d2d562fa50224c2186
Ryan Berdeen <ryan@ryanberdeen.com>
2021-05-23 23:17:44 -0400

    refresh strava oauth token

--- tools/strava-login.js
+++ tools/strava-login.js
@@ -7,19 +7,24 @@ import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 
-const client_secret = 'SECRET';
+export const client_secret = 'SECRET';
+export const client_id = 6464;
+
+export function writeCreds(body) {
+  fs.writeFileSync(
+    path.join(__dirname, '..', 'creds', 'strava.json'),
+    JSON.stringify(body)
+  );
+  console.log(body);
+}
 
 export default function login({ _: code }) {
   axios
     .post(
-      `https://www.strava.com/api/v3/oauth/token?client_id=6464&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`
+      `https://www.strava.com/api/v3/oauth/token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`
     )
     .then(({ data: body }) => {
-      fs.writeFileSync(
-        path.join(__dirname, '..', 'creds', 'strava.json'),
-        JSON.stringify(body)
-      );
-      console.log(body);
+      writeCreds(body);
     })
     .catch((result) => {
       console.error('error', result.stack || JSON.stringify(result));
