97d68e8843c9a1319ea08f3e7a4ceef0bc92bfa2
Ryan Berdeen <ryan@ryanberdeen.com>
2021-05-29 03:17:42 -0400

    add a table with links to openstreetmap

--- app/components/pages/WayDetails.tsx
+++ app/components/pages/WayDetails.tsx
@@ -35,6 +35,7 @@ export default function WayDetails({ way }: { way: GroupedWays }) {
           {() => <Ways features={way.features} selected={true} />}
         </MapComponent>
       </MapBox>
+
       <h2>Intersections</h2>
       <WayListColumns>
         {Array.from(intersections)
@@ -47,6 +48,32 @@ export default function WayDetails({ way }: { way: GroupedWays }) {
             </li>
           ))}
       </WayListColumns>
+
+      <h2>OpenStreetMap Ways</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>One Way</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {way.features.map((f) => (
+            <tr key={f.properties.id}>
+              <td>
+                <a href={`https://www.openstreetmap.org/${f.properties.id}`}>
+                  {f.properties.displayName}
+                </a>
+              </td>
+              <td>{f.properties.highway}</td>
+              <td>{f.properties.oneway}</td>
+              <td>{f.properties.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
