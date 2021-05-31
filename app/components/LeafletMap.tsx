3eb57aadf584fd4cbfae11af390eca3197b2f583
Ryan Berdeen <ryan@ryanberdeen.com>
2021-05-31 17:13:02 -0400

    zoom much further

--- app/components/LeafletMap.tsx
+++ app/components/LeafletMap.tsx
@@ -18,7 +18,7 @@ export default function LeafletMap({
         {
           attribution:
             'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
+          maxZoom: 22,
           id: 'mapbox/streets-v11',
           tileSize: 512,
           zoomOffset: -1,
