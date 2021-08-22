2660074263124c61e375b079e27a9c8b3caac1ce
Ryan Berdeen <ryan@ryanberdeen.com>
2021-08-22 13:37:20 -0400

    udpate webpack etc

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,10 @@ module.exports = {
     },
   },
   devServer: {
-    publicPath: '/build',
+    devMiddleware: { publicPath: '/build' },
+    static: {
+      directory: __dirname,
+    },
     https: {
       key: fs.readFileSync(path.join(__dirname, 'key.pem')),
       cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
