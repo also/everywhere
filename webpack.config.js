ec5775c424bb9ff03325511f3d2905e45fa878df
Ryan Berdeen <ryan@ryanberdeen.com>
2021-08-22 16:24:38 -0400

    don't require cert for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => ({
   context: __dirname + '/app',
   entry: './entry',
   output: {
@@ -56,30 +56,32 @@ module.exports = {
       'compact-json$': 'compact-json-loader',
     },
   },
-  devServer: {
-    devMiddleware: { publicPath: '/build' },
-    static: {
-      directory: __dirname,
-    },
-    https: {
-      key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
-    },
-    onBeforeSetupMiddleware(devServer) {
-      devServer.app.get('/strava-auth', async (req, res) => {
-        try {
-          require('ts-node').register({ transpileOnly: true });
-          const { loginHandler } = require('./tools/strava-creds');
-          const redirectUrl = await loginHandler(req.query);
-          if (redirectUrl) {
-            res.redirect(redirectUrl);
-          } else {
-            res.send('ok');
-          }
-        } catch (e) {
-          res.status(500).send(e.stack);
-        }
-      });
-    },
-  },
-};
+  devServer: env.WEBPACK_SERVE
+    ? {
+        devMiddleware: { publicPath: '/build' },
+        static: {
+          directory: __dirname,
+        },
+        https: {
+          key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+          cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+        },
+        onBeforeSetupMiddleware(devServer) {
+          devServer.app.get('/strava-auth', async (req, res) => {
+            try {
+              require('ts-node').register({ transpileOnly: true });
+              const { loginHandler } = require('./tools/strava-creds');
+              const redirectUrl = await loginHandler(req.query);
+              if (redirectUrl) {
+                res.redirect(redirectUrl);
+              } else {
+                res.send('ok');
+              }
+            } catch (e) {
+              res.status(500).send(e.stack);
+            }
+          });
+        },
+      }
+    : undefined,
+});
