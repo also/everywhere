eabd721f974f8a2ec828214feee1f17e02db8ac0
Ryan Berdeen <ryan@ryanberdeen.com>
2021-08-29 11:21:51 -0400

    find trips with lots of points around the end

---
+++ tools/find-junk-trip-data.ts
@@ -0,0 +1,42 @@
+import { MultiLineString } from 'geojson';
+import { positionDistance } from '../app/distance';
+import { feature } from '../app/geo';
+import { stravaTopologies } from './strava';
+
+import minHeap from '../app/min-heap';
+import { RawStravaTripFeature, RawStravaTripProperties } from '../app/trips';
+
+export default async function () {
+  // TODO heap max size?
+  const heap = minHeap<{ feat: RawStravaTripFeature; count: number }>(
+    (a, b) => b.count - a.count
+  );
+
+  for (const trip of stravaTopologies()) {
+    const feat = feature<MultiLineString, RawStravaTripProperties>(trip);
+    const lastCoords =
+      feat.geometry.coordinates[feat.geometry.coordinates.length - 1];
+    const lastCoord = lastCoords[lastCoords.length - 1];
+    let count = 0;
+    feat.geometry.coordinates.forEach((coords) => {
+      coords.forEach((coord) => {
+        const dist = positionDistance(lastCoord, coord);
+        if (dist < 15) {
+          count++;
+        }
+      });
+    });
+    // if (feat.id === 1238347896) {
+    heap.push({ feat, count });
+    // }
+  }
+
+  for (let i = 0; i < 30; i++) {
+    const entry = heap.pop();
+    if (!entry) {
+      break;
+    }
+    const { feat, count } = entry;
+    console.log(`${feat.id} ${feat.properties.activity.type} ${count}`);
+  }
+}
--- tools/strava.ts
+++ tools/strava.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { SimpleTopology } from './topojson-utils';
+import { RawStravaTripProperties } from '../app/trips';
 
 const appDataPath = path.join(__dirname, '../app-data/');
 const tripsPath = path.join(appDataPath, 'strava-trips');
@@ -9,9 +10,9 @@ export function* stravaTopologies() {
   for (const tripFile of fs.readdirSync(tripsPath)) {
     const match = tripFile.match(/strava-(\d+)\.geojson/);
     if (match) {
-      const trip: SimpleTopology = JSON.parse(
-        fs.readFileSync(path.join(tripsPath, tripFile), 'utf8')
-      );
+      const trip: SimpleTopology<
+        TopoJSON.GeometryObject<RawStravaTripProperties>
+      > = JSON.parse(fs.readFileSync(path.join(tripsPath, tripFile), 'utf8'));
 
       yield trip;
     }
