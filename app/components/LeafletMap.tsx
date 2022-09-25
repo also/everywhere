79e3f9d360c85d803e56991ff6d5f6206032e9e0
Ryan Berdeen <ryan@ryanberdeen.com>
2022-09-25 15:55:09 -0400

    multi-layer map with controls

--- app/components/LeafletMap.tsx
+++ app/components/LeafletMap.tsx
@@ -1,6 +1,66 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import React from 'react';
+
+const heatmapOpts = (() => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return require('../../creds/heatmaps.json') as {
+      title: string;
+      url: string;
+      opts: Record<string, any>;
+    }[];
+  } catch (e) {
+    return [];
+  }
+})();
+
+const mapboxOpts = {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 22,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken:
+    'pk.eyJ1IjoicmJlcmRlZW4iLCJhIjoiZTU1YjNmOWU4MWExNDJhNWNlMTAxYjA2NjFlODBiNWUifQ.AHJ0I8wQi1pJekXfAaPxLw',
+};
+
+function createMap(element: HTMLElement) {
+  const streets = L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    mapboxOpts
+  );
+
+  const satellite = L.tileLayer(
+    'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.jpg90?access_token={accessToken}',
+    mapboxOpts
+  );
+
+  const control = L.control.layers();
+  control.addBaseLayer(streets, 'Streets');
+  control.addBaseLayer(satellite, 'Satellite');
+
+  const heatmaps = heatmapOpts.map((opts) => {
+    const heatmap = L.tileLayer(opts.url, opts.opts);
+    control.addOverlay(heatmap, opts.title);
+    return heatmap;
+  });
+
+  const map = L.map(element!, {
+    layers: [
+      streets,
+      ...heatmaps.filter((l) => (l.options as any).enabledByDefault !== false),
+    ],
+  }).setView([42.389118, -71.097153], 11);
+
+  control.addTo(map);
+
+  return {
+    map,
+    defaultLayers: new Set([streets, satellite, ...heatmaps]),
+  };
+}
 
 export default React.memo(function LeafletMap({
   features = [],
@@ -10,44 +70,22 @@ export default React.memo(function LeafletMap({
   customize?(map: L.Map): void;
 }) {
   const mapComponent = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<L.Map>();
-
-  const tiles = useMemo(
-    () =>
-      L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 22,
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken:
-            'pk.eyJ1IjoicmJlcmRlZW4iLCJhIjoiZTU1YjNmOWU4MWExNDJhNWNlMTAxYjA2NjFlODBiNWUifQ.AHJ0I8wQi1pJekXfAaPxLw',
-        }
-      ),
-    []
-  );
+  const mapRef = useRef<{ map: L.Map; defaultLayers: Set<L.Layer> }>();
 
   useEffect(() => {
     if (!mapRef.current) {
-      mapRef.current = L.map(mapComponent.current!).setView(
-        [42.389118, -71.097153],
-        11
-      );
-
-      tiles.addTo(mapRef.current);
+      mapRef.current = createMap(mapComponent.current!);
     }
-    const map = mapRef.current;
+
+    const { map, defaultLayers } = mapRef.current;
     map.eachLayer((layer) => {
-      if (layer !== tiles) {
+      if (!defaultLayers.has(layer)) {
         map.removeLayer(layer);
       }
     });
 
     if (customize) {
-      customize(mapRef.current);
+      customize(map);
     }
 
     features.forEach((f) =>
