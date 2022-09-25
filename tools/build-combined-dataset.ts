49f4123c20896cc9bb10dd7960e6896c4dcbef55
Ryan Berdeen <ryan@ryanberdeen.com>
2022-09-25 13:12:23 -0400

    clean up combined dataset generation

--- tools/build-combined-dataset.ts
+++ tools/build-combined-dataset.ts
@@ -1,45 +1,9 @@
-import fs from 'fs';
-import path from 'path';
 import { stravaTopologies } from './strava';
 
-import { combineTolologies, SimpleTopology } from './topojson-utils';
+import { combineTolologies } from './topojson-utils';
 
 export default function () {
   const bigTopo = combineTolologies(stravaTopologies());
-
-  // const videoDataPath = path.join(appDataPath, 'video-metadata');
-  // const videos = [];
-  // const videoTrips: Feature[] = [];
-
-  // for (const videoFile of fs.readdirSync(videoDataPath)) {
-  //   const filename = fs.readFileSync(
-  //     path.join(videoDataPath, videoFile),
-  //     'utf8'
-  //   );
-  //   const video = JSON.parse(filename);
-  //   const match = videoFile.match(/(.+)\.json/);
-  //   if (match) {
-  //     video.name = match[1] + '.MP4';
-  //     videos.push(video);
-  //   } else {
-  //     const geoJson = video as Feature;
-
-  //     if (
-  //       geoJson.geometry.type !== 'Point' &&
-  //       geoJson.geometry.type !== 'GeometryCollection' &&
-  //       geoJson.geometry.coordinates.length > 0
-  //     ) {
-  //       const topology = topojson.topology({ geoJson });
-  //       addTopology(bigTopo, topology);
-  //     }
-  //     videoTrips.push(geoJson);
-  //   }
-  // }
-
-  // const featureColl: FeatureCollection = {
-  //   type: 'FeatureCollection',
-  //   features: videoTrips,
-  // };
 
   console.log(JSON.stringify(bigTopo, null, 2));
 }
---
+++ tools/build-combined-video-dataset.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+import { Feature } from 'geojson';
+import * as topojson from 'topojson';
+import { combineTolologies, SimpleTopology } from './topojson-utils';
+
+const appDataPath = path.join(__dirname, '../app-data/');
+
+function* videoGeoJson() {
+  const videoDataPath = path.join(appDataPath, 'video-metadata');
+
+  for (const basename of fs.readdirSync(videoDataPath)) {
+    if (basename.endsWith('.geojson')) {
+      const filename = path.join(videoDataPath, basename);
+      yield {
+        filename,
+        geoJson: JSON.parse(fs.readFileSync(filename, 'utf8')) as Feature,
+      };
+    }
+  }
+}
+
+function* videoTopoJson() {
+  for (const { filename, geoJson } of videoGeoJson()) {
+    if (
+      geoJson.geometry.type === 'LineString' &&
+      geoJson.geometry.coordinates.length === 0
+    ) {
+      // skip empty lines - topojson doesn't like them
+      // TODO include records of videos with no location data in the combined dataset somehow?
+      continue;
+    }
+    try {
+      yield topojson.topology({ geoJson }) as SimpleTopology;
+    } catch (e) {
+      console.error(`error processing ${filename}`);
+    }
+  }
+}
+
+export default function () {
+  const bigTopo = combineTolologies(videoTopoJson());
+
+  console.log(JSON.stringify(bigTopo, null, 2));
+}
