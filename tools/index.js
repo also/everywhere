2d0c64ae1047735a0e46c8846020534158a73dde
Ryan Berdeen <ryan@ryanberdeen.com>
2022-09-24 20:25:59 -0400

    need different module option for ts-node

--- tools/index.js
+++ tools/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-require('ts-node').register({ transpileOnly: true });
+require('ts-node').register({
+  transpileOnly: true,
+  compilerOptions: { module: 'commonjs' },
+});
 
 require('./main.js').default();
