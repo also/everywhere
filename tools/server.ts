555ceb81f7480888e7251c63e878fe8abe81cf84
Ryan Berdeen <ryan@ryanberdeen.com>
2023-04-03 00:43:08 -0400

    simple server for videos on my local network
    binds to 0.0.0.0 and does some minor validation of requests, so this would become a security risk if used for anything interesting or run constandly

---
+++ tools/server.ts
@@ -0,0 +1,30 @@
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+import send from 'send';
+
+export default function ({ _: directories }: { _: string[] }) {
+  for (const dir of directories) {
+    if (!fs.existsSync(dir)) {
+      console.error('Directory does not exist:', dir);
+      process.exit(1);
+    }
+  }
+  const server = http.createServer(function onRequest(req, res) {
+    if (req.url?.match(/^\/[A-Za-z0-9]+\.MP4$/)) {
+      for (const dir of directories) {
+        const filename = path.join(dir, req.url);
+        if (fs.existsSync(filename)) {
+          console.log('send', filename);
+          send(req, filename).pipe(res);
+          return;
+        }
+      }
+    }
+    console.log('404', req.url);
+    res.statusCode = 404;
+    res.end();
+  });
+
+  server.listen(3000, '0.0.0.0');
+}
