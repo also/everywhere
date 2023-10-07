a0762fc034da7a36d43beba9ae80f6bd157051b3
Ryan Berdeen <ryan@ryanberdeen.com>
2023-10-07 13:43:38 -0400

    video in docs

---
+++ app/docs/VideoExample.tsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import DataSetContext from '../components/DataSetContext';
+import VideoPlayer from '../components/VideoPlayer';
+
+export default function VideoExample({ name }: { name: string }) {
+  const dataSet = useContext(DataSetContext);
+  const video = dataSet.videos.get(name);
+  if (!video) {
+    return <div>Video not found</div>;
+  }
+  return <VideoPlayer video={video} seek={0} onLocationChange={() => {}} />;
+}
