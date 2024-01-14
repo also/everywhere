fe4c189689fc1e1c81c4fb0e43b73fa20709123d
Ryan Berdeen <ryan@ryanberdeen.com>
2024-01-14 13:18:06 -0800

    filter non-gpx

--- tools/build-combined-alltrails-dataset.ts
+++ tools/build-combined-alltrails-dataset.ts
@@ -5,7 +5,9 @@ import { topology } from 'topojson-server';
 import { combineTopologies, SimpleTopology } from './topojson-utils';
 
 export default function () {
-  const files = fs.readdirSync('data/alltrails-recordings');
+  const files = fs
+    .readdirSync('data/alltrails-recordings')
+    .filter((f) => f.endsWith('.gpx'));
   const topologies: SimpleTopology[] = [];
   for (const file of files) {
     const xml = fs.readFileSync(`data/alltrails-recordings/${file}`, 'utf8');
