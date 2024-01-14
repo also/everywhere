5de06cd29d6567cb00f0fb321321c6109b184f89
Ryan Berdeen <ryan@ryanberdeen.com>
2024-01-14 12:52:06 -0800

    alltrails (really just gpx) dataset

---
+++ tools/build-combined-alltrails-dataset.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+import { DOMParser } from 'xmldom';
+import { gpx } from '@tmcw/togeojson';
+import { topology } from 'topojson-server';
+import { combineTopologies, SimpleTopology } from './topojson-utils';
+
+export default function () {
+  const files = fs.readdirSync('data/alltrails-recordings');
+  const topologies: SimpleTopology[] = [];
+  for (const file of files) {
+    const xml = fs.readFileSync(`data/alltrails-recordings/${file}`, 'utf8');
+    const doc = new DOMParser().parseFromString(xml);
+    const geoJson = gpx(doc);
+    const topo = topology({ geoJson }) as SimpleTopology;
+    topologies.push(topo);
+  }
+  console.log(
+    JSON.stringify(
+      combineTopologies(topologies, () => ({ type: 'alltrails-recording' }))
+    )
+  );
+}
