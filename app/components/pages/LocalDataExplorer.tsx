2f90670b67b1b1e0f3fdcf257ed4611a2efc9513
Ryan Berdeen <ryan@ryanberdeen.com>
2024-02-11 18:48:00 -0500

    split file handling

--- app/components/pages/LocalDataExplorer.tsx
+++ app/components/pages/LocalDataExplorer.tsx
@@ -8,15 +8,7 @@ import PageTitle from '../PageTitle';
 import MapComponent from '../Map';
 import MapContext from '../MapContext';
 import MapBox from '../MapBox';
-import { SeekableBlobBuffer } from '../../../tools/parse/buffers';
-import { extractGps } from '../../../tools/parse/gopro-gps';
-import { bind, fileRoot, Traverser } from '../../../tools/parse';
-import { Box, parser as mp4Parser } from '../../../tools/parse/mp4';
-import { getMeta, Metadata } from '../../../tools/parse/gpmf';
-import { FeatureOrCollection, features, singleFeature } from '../../geo';
 import { DataSet } from '../../data';
-import { buildDataSet, RawStravaTripFeature } from '../../trips';
-import { RawVideoFeature, toChapter, VideoChapter } from '../../videos';
 import LeafletMap from '../LeafletMap';
 import Table from '../Table';
 import TraverserView, { GpmfSamples } from '../data/TraverserView';
@@ -26,28 +18,21 @@ import StandardPage from '../StandardPage';
 import { VectorTileFileView } from '../VectorTileFileView';
 import { NavExtension } from '../Nav';
 import LoadingPage from './LoadingPage';
-import { CompleteActivity } from '../../../tools/strava-api';
-import { completeActivityToGeoJson } from '../../../tools/strava';
 import Map from '../Map';
-
-interface FileWithDetails {
-  file: FileWithHandle;
-  inferredType?: string;
-}
+import {
+  FileWithDetails,
+  peekFile,
+  readFile,
+  readFiles,
+  readToDataset,
+  SomeFile,
+} from '../../file-data';
 
 function Path({ feature }: { feature: Feature }) {
   const { path } = useContext(MapContext);
 
   return <path className="trip" d={path(feature)} />;
 }
-
-type SomeFile = {
-  geojson: Feature | FeatureCollection;
-  json?: any;
-  mp4?: Traverser<Box>;
-  track?: Metadata;
-  raw: FileWithHandle;
-};
 
 function DataView({ file }: { file: FileWithDetails }) {
   const loaded = useMemoAsync<SomeFile>(() => readFile(file.file), [file]);
@@ -144,103 +129,6 @@ function DataSetLoader({
   } else {
     return <Map width={600} height={600} asLoadingAnimation={true} />;
   }
-}
-
-function isProbablyStravaCompleteActivity(json: any): json is CompleteActivity {
-  return json.activity && json.streams;
-}
-
-async function readFile(file: File): Promise<SomeFile> {
-  let geojson: Feature | FeatureCollection | undefined;
-  let mp4;
-  let track;
-  let json;
-  if (file.name.toLowerCase().endsWith('.mp4')) {
-    const data = new SeekableBlobBuffer(file, 1024000);
-    mp4 = bind(mp4Parser, data, fileRoot(data));
-    track = await getMeta(mp4);
-    geojson = await extractGps(track, mp4);
-  } else {
-    const text = await file.text();
-    json = JSON.parse(text);
-    if (isProbablyStravaCompleteActivity(json)) {
-      geojson = completeActivityToGeoJson(json);
-    } else if (json.type === 'Topology') {
-      geojson = features(json);
-    } else {
-      geojson = json as Feature;
-    }
-  }
-  if (!geojson) {
-    geojson = {
-      type: 'FeatureCollection',
-      features: [],
-    };
-  }
-  (geojson.type === 'FeatureCollection' ? geojson.features : [geojson]).forEach(
-    (feat) => {
-      if (!feat.properties) {
-        feat.properties = {};
-      }
-      feat.properties.filename = file.name;
-    }
-  );
-  return { json, geojson, mp4, track, raw: file };
-}
-
-function readFiles(files: FileWithDetails[]): Promise<SomeFile[]> {
-  return Promise.all(files.map((file) => readFile(file.file)));
-}
-
-function isProbablyStravaTrip(
-  f: FeatureOrCollection<any, any>
-): f is RawStravaTripFeature {
-  return f.type === 'Feature' && !!f.properties?.activity?.moving_time;
-}
-
-function isProbablyVideoTrack(
-  f: FeatureOrCollection<any, any>
-): f is RawVideoFeature {
-  return f.type === 'Feature' && !!f.properties?.creationTime;
-}
-
-function readToDataset(newFiles: SomeFile[]) {
-  const trips: RawStravaTripFeature[] = [];
-
-  const videoChapters: VideoChapter[] = [];
-  newFiles.forEach(({ geojson, raw: { name } }) => {
-    const f = singleFeature(geojson) || geojson;
-    if (isProbablyStravaTrip(f)) {
-      trips.push(f);
-    } else if (isProbablyVideoTrack(f)) {
-      const start = new Date(f.properties.creationTime * 1000);
-      // using current timezone :(
-      start.setMinutes(start.getMinutes() + start.getTimezoneOffset());
-      videoChapters.push(
-        toChapter(name, {
-          start: +start,
-          // TODO what the hell? 90
-          // this gives the right duration for GOPR0039, at least
-          duration: f.properties.duration / 1000 / 1000 / 90,
-        })
-      );
-    }
-  });
-
-  return buildDataSet(trips, videoChapters);
-}
-
-async function peekFile(file: FileWithHandle) {
-  const head = file.slice(0, 100);
-  const value = await head.arrayBuffer();
-  const array = new Uint8Array(value);
-  // if json
-  if (array[0] === 123) {
-    const s = new TextDecoder('ascii').decode(array);
-    const type = s.match(/{\s*"type"\s*:\s*"([^"]+)"/)?.[1];
-    return type ?? 'json';
-  }
-  return 'unknown';
 }
 
 export default function LocalDataExplorer({
---
+++ app/file-data.ts
@@ -0,0 +1,123 @@
+import { Feature, FeatureCollection } from 'geojson';
+import { FileWithHandle } from 'browser-fs-access';
+import { SeekableBlobBuffer } from '../tools/parse/buffers';
+import { extractGps } from '../tools/parse/gopro-gps';
+import { bind, fileRoot, Traverser } from '../tools/parse';
+import { Box, parser as mp4Parser } from '../tools/parse/mp4';
+import { getMeta, Metadata } from '../tools/parse/gpmf';
+import { FeatureOrCollection, features, singleFeature } from './geo';
+import { DataSet } from './data';
+import { buildDataSet, RawStravaTripFeature } from './trips';
+import { RawVideoFeature, toChapter, VideoChapter } from './videos';
+import { CompleteActivity } from '../tools/strava-api';
+import { completeActivityToGeoJson } from '../tools/strava';
+
+export interface FileWithDetails {
+  file: FileWithHandle;
+  inferredType?: string;
+}
+
+export type SomeFile = {
+  geojson: Feature | FeatureCollection;
+  json?: any;
+  mp4?: Traverser<Box>;
+  track?: Metadata;
+  raw: FileWithHandle;
+};
+
+function isProbablyStravaCompleteActivity(json: any): json is CompleteActivity {
+  return json.activity && json.streams;
+}
+
+export async function readFile(file: File): Promise<SomeFile> {
+  let geojson: Feature | FeatureCollection | undefined;
+  let mp4;
+  let track;
+  let json;
+  if (file.name.toLowerCase().endsWith('.mp4')) {
+    const data = new SeekableBlobBuffer(file, 1024000);
+    mp4 = bind(mp4Parser, data, fileRoot(data));
+    track = await getMeta(mp4);
+    geojson = await extractGps(track, mp4);
+  } else {
+    const text = await file.text();
+    json = JSON.parse(text);
+    if (isProbablyStravaCompleteActivity(json)) {
+      geojson = completeActivityToGeoJson(json);
+    } else if (json.type === 'Topology') {
+      geojson = features(json);
+    } else {
+      geojson = json as Feature;
+    }
+  }
+  if (!geojson) {
+    geojson = {
+      type: 'FeatureCollection',
+      features: [],
+    };
+  }
+  (geojson.type === 'FeatureCollection' ? geojson.features : [geojson]).forEach(
+    (feat) => {
+      if (!feat.properties) {
+        feat.properties = {};
+      }
+      feat.properties.filename = file.name;
+    }
+  );
+  return { json, geojson, mp4, track, raw: file };
+}
+
+export function readFiles(files: FileWithDetails[]): Promise<SomeFile[]> {
+  return Promise.all(files.map((file) => readFile(file.file)));
+}
+
+function isProbablyStravaTrip(
+  f: FeatureOrCollection<any, any>
+): f is RawStravaTripFeature {
+  return f.type === 'Feature' && !!f.properties?.activity?.moving_time;
+}
+
+function isProbablyVideoTrack(
+  f: FeatureOrCollection<any, any>
+): f is RawVideoFeature {
+  return f.type === 'Feature' && !!f.properties?.creationTime;
+}
+
+export function readToDataset(newFiles: SomeFile[]): DataSet {
+  const trips: RawStravaTripFeature[] = [];
+
+  const videoChapters: VideoChapter[] = [];
+  newFiles.forEach(({ geojson, raw: { name } }) => {
+    const f = singleFeature(geojson) || geojson;
+    if (isProbablyStravaTrip(f)) {
+      trips.push(f);
+    } else if (isProbablyVideoTrack(f)) {
+      const start = new Date(f.properties.creationTime * 1000);
+      // using current timezone :(
+      start.setMinutes(start.getMinutes() + start.getTimezoneOffset());
+      videoChapters.push(
+        toChapter(name, {
+          start: +start,
+          // TODO what the hell? 90
+          // this gives the right duration for GOPR0039, at least
+          duration: f.properties.duration / 1000 / 1000 / 90,
+        })
+      );
+    }
+  });
+
+  return buildDataSet(trips, videoChapters);
+}
+
+export async function peekFile(file: FileWithHandle) {
+  const head = file.slice(0, 100);
+  const value = await head.arrayBuffer();
+  const array = new Uint8Array(value);
+  // if json
+  if (array[0] === 123) {
+    const s = new TextDecoder('ascii').decode(array);
+    const type = s.match(/{\s*"type"\s*:\s*"([^"]+)"/)?.[1];
+    return type ?? 'json';
+  }
+  return 'unknown';
+}
