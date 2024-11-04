e8865cb3355e44c06bff4e9840987578e1869249
Ryan Berdeen <ryan@ryanberdeen.com>
2024-11-03 19:07:55 -0500

    throw error when worker fails to start

--- app/worker-stuff.ts
+++ app/worker-stuff.ts
@@ -45,7 +45,19 @@ export const lookup =
 export async function create() {
   const worker = new Worker(new URL('./tile-worker.ts', import.meta.url));
 
+  const error = new Promise<Error>((__, reject) => {
+    worker.addEventListener(
+      'error',
+      (e) => {
+        reject(new Error(`Failed to start worker`));
+      },
+      { once: true }
+    );
+  });
+
   const channel = WorkerChannel.forWorker(worker);
-  await channel.sendRequest(workerHandshake, 'ping');
-  return { worker, channel };
+  return Promise.race([
+    channel.sendRequest(workerHandshake, 'ping'),
+    error,
+  ]).then(() => ({ worker, channel }));
 }
