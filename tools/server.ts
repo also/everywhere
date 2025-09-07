f3b1b46e37b776ad2f916880efc6ad31911a82bb
Ryan Berdeen <ryan@ryanberdeen.com>
2025-09-07 17:12:14 -0400

    tailwind

--- tools/server.ts
+++ tools/server.ts
@@ -1,16 +1,23 @@
 import http from 'http';
+import https from 'https';
 import path from 'path';
 import fs from 'fs';
 import send from 'send';
 
-export default function ({ _: directories }: { _: string[] }) {
+export default function ({ _: [cert, ...directories] }: { _: string[] }) {
+  const options = {
+    key: fs.readFileSync(`${cert}.key`),
+    cert: fs.readFileSync(`${cert}.cer`),
+  };
+
   for (const dir of directories) {
     if (!fs.existsSync(dir)) {
       console.error('Directory does not exist:', dir);
       process.exit(1);
     }
   }
-  const server = http.createServer(function onRequest(req, res) {
+
+  function onRequest(req: http.IncomingMessage, res: http.ServerResponse) {
     if (req.url?.match(/^\/[A-Za-z0-9]+\.MP4$/)) {
       for (const dir of directories) {
         const filename = path.join(dir, req.url);
@@ -23,7 +30,8 @@ export default function ({ _: directories }: { _: string[] }) {
     console.log('404', req.url);
     res.statusCode = 404;
     res.end();
-  });
+  }
+  http.createServer(onRequest).listen(3000, '0.0.0.0');
 
-  server.listen(3000, '0.0.0.0');
+  https.createServer(options, onRequest).listen(443);
 }
